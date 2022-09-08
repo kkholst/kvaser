b8061f3e03391e821c20923bd2020b52ea7590ca
Klaus Holst <klaus@holst.it>
2022-09-08 07:35:08 +0200

    split code in modules. Add dist. to exogenous variables

--- src/kvaser/__init__.py
+++ src/kvaser/__init__.py
@@ -3,5 +3,6 @@ from .data import getdata  # noqa: F401
 from .__utils__ import filesize, desc  # noqa: F401
 from .__about__ import __version__  # noqa: F401
 from .blob_storage import blob_storage # noqa F401
-from .sim import normal, bernoulli, poisson, discrete, Dist, dag
+from .dist import normal, bernoulli, poisson, discrete, Dist
+from .dag import dag
 from .reload import reload  # noqa F401
--- src/kvaser/dag.py
+++ src/kvaser/dag.py
@@ -1,122 +1,15 @@
-#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Simulation class
+# Copyright (c) 2019-2022 Klaus K. Holst.  All rights reserved.
 
-import numpy as np
+import kvaser as kv
 import networkx as nx
+import numpy as np
 import pandas as pd
 from PIL import Image
 from io import BytesIO
 import pydot
-
-class Dist:
-    r"""Probability distribution class.
-    Super class, not to be called directly
-    """
-
-    rng = np.random.default_rng()
-    name = 'Generic'
-    meanpar = 'loc'
-
-    def gen(self, size, **kwargs):
-        return None
-
-    @staticmethod
-    def invlink(x):
-        return np.array(x)
-
-    def __init__(self, seed=None, **kwargs):
-        r"""Distribution class constructor
-
-        Parameters
-        ----------
-        seed: int64
-              Random seed (optional)
-        **kwargs:
-              Extra arguments passed to random generator
-
-        Returns
-        ----------
-        Dist
-           Dist object
-        """
-        self.kparam = kwargs
-        self.rng = np.random.default_rng(seed)
-
-    def simulate(self, mean=None, lp=None):
-        r"""Simulation method
-
-        Parameters
-        ----------
-        mean:
-
-        Returns
-        ----------
-
-        """
-        if lp is not None:
-            mean = self.invlink(lp)
-        par = {}
-        par[self.meanpar] = mean
-
-        return self.gen(**par, **self.kparam, size=len(mean))
-
-    def __repr__(self):
-        return self.name + ' distribution ' + \
-            str(self.kparam)
-
-    def __str__(self):
-        return self.name + ' distribution ' + \
-            str(self.kparam)
-
-
-class normal(Dist):
-    r"""Normal distribution class
-    constructor arguments:
-    loc: float or array_like of floats
-         Mean
-    scale: float or array_like of floats
-         Standard deviation
-    """
-    def gen(self, size, **kwargs):
-        return self.rng.normal(size=size, **kwargs)
-    name = 'Normal'
-    meanpar = 'loc'
-
-
-class bernoulli(Dist):
-    name = 'Binomial'
-    meanpar = 'p'
-
-    def gen(self, size, **kwargs):
-        return self.rng.binomial(size=size, n=1, **kwargs)
-
-    @staticmethod
-    def invlink(x):
-        return(1/(1+np.exp(-np.array(x))))
-
-
-class poisson(Dist):
-    name = 'Poisson'
-    meanpar = 'lam'
-    def gen(self, size, **kwargs):
-        return self.rng.poisson(size=size, **kwargs)
-
-    @staticmethod
-    def invlink(x):
-        return(np.exp(np.array(x)))
-
-
-class discrete(Dist):
-    name = 'Discrete'
-
-    def __init__(self, values=[0,1], p=[0.5,0.5], **kwargs):
-        super().__init__()
-        self.values = np.array(values)
-        self.p = np.array(p)
-
-    def gen(self, size, **kwargs):
-        return self.rng.choice(a=self.values, p=self.p, size=size)
-
-
 
 class dag:
     r"""DAG model class
@@ -130,11 +23,14 @@ class dag:
 
     def regression(self, y, x=[]):
         self.G.add_node(y)
-        self.distribution(y)
+        if y not in self.var.keys():
+            self.distribution(y)
         for v in x:
             self.G.add_edge(v, y)
+            if v not in self.var.keys():
+                self.distribution(v)
 
-    def distribution(self, y, generator=normal()):
+    def distribution(self, y, generator=kv.normal()):
         r"""Set distribution of variable"""
         self.G.add_node(y)
         self.var[y] = generator
